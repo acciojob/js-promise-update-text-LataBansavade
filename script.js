5a45998880245880a897c8d16a7c955a27dbe466
LataBansavade <139533645+LataBansavade@users.noreply.github.com>
2023-08-28 13:25:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+
+function delayedHello() {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve("Hello, world!");
+        }, 1000); // 1 second delay
+      });
+    }
+
+    // Call the delayedHello function and update the content of the output div
+    delayedHello()
+      .then(result => {
+        const outputDiv = document.getElementById('output');
+        outputDiv.textContent = result;
+      })
+      .catch(error => {
+        console.error('Promise rejected:', error);
+      });
